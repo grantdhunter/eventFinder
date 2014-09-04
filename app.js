4661714d8beb2f39dca825f980e8ed4c354ce8a2
grantdhunter <gr@nt-hunter.ca>
2014-09-04 10:51:00 -0600

    Works for most of the sample sets
    It can find events by finding similar urls on the site.

--- app.js
+++ app.js
@@ -8,21 +8,18 @@ var levenshtein = require('./levenshtein.js');
 //index 1: app.js
 //index 2: first parameter which will be the url of the site to be parse
 var givenLink = process.argv[2]
-console.log(givenLink);
+var baseUrl = url.parse(givenLink).protocol + '//' + url.parse(givenLink).hostname;
+console.log('givenLink: ' + givenLink);
+console.log('baseUrl: ' + baseUrl);
 getPage(givenLink);
 
 
-function crawlUp(givenUrl) {
-    givenUrl = givenUrl.split('/');
-    givenUrl.pop();
-    givenUrl = givenUrl.join('/');
-    return givenUrl;
-}
 
 function getPage(link) {
-    link = crawlUp(link);
-    request(link, function (error, response, body) {
-        console.log(link);
+    var destinationLink = crawlUp(link);
+    console.log('destination: ' + destinationLink);
+    request(destinationLink, function (error, response, body) {
+
         if (error) {
             console.log(error);
             return;
@@ -32,52 +29,82 @@ function getPage(link) {
 
         if (response.statusCode === 200) {
             var $ = cheerio.load(body)
+
             $('a').each(function (i, elem) {
-                //ignore external links
-                if ($(elem).attr('href').indexOf('http') < 0) {
-                    links.push(getDistance(link + $(elem).attr('href')));
-                } else {
-                    links.push(getDistance($(elem).attr('href')));
-                }
+                links.push(getDistance(normalizeUrl($(elem).attr('href'))));
             });
 
-            links = links.sort(function (a, b) {
-                if (a.distance > b.distance) {
-                    return 1;
-                } else {
-                    return -1;
-                }
-            });
+            links = getBestResults(links, 10);
+            displayResults(links);
 
-            if (links.length > 10) {
-                var maxResults = 10;
-            } else {
-                var maxResults = links.length;
-            }
-
-            console.log('/************Results***********/');
-            for (var i = 0; i < maxResults; i++) {
-                console.log(links[i].link);
-            }
         } else {
-            getPage(link);
+            console.log('StatusCode: ' + response.statusCode);
+            getPage(destinationLink);
         }
     });
 }
 
+function crawlUp(givenUrl) {
+    givenUrl = givenUrl.split('/');
 
+    if (givenUrl[givenUrl.length - 1] === '') {
+        givenUrl.pop();
+    }
+    givenUrl.pop();
+    givenUrl = givenUrl.join('/');
+    return givenUrl;
+}
 
 function getDistance(eventLink) {
 
-    var distance = levenshtein.getEditDistance(crawlUp(givenLink), eventLink);
-    console.log(eventLink);
+    console.log('givenLink: ' + givenLink);
+    console.log('eventLink: ' + eventLink);
+
+    //var distance = levenshtein.getEditDistance(crawlUp(givenLink), crawlUp(eventLink));
+    var distance = levenshtein.getEditDistance(givenLink, eventLink);
+    console.log('distance:  ' + distance);
     return {
         link: eventLink,
         distance: distance
     }
 }
 
+function getBestResults(links, maxNumber) {
+    links = links.sort(function (a, b) {
+        if (a.distance > b.distance) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
 
-function forwardFilter(link, eventLink) {
+    if (links.length > maxNumber) {
+        var maxResults = maxNumber;
+    } else {
+        var maxResults = links.length;
+    }
 
+    return links.slice(0, maxResults);
+}
+
+function displayResults(links) {
+    console.log('/************Results***********/');
+    for (var i = 0; i < links.length; i++) {
+        console.log(links[i].link);
+    }
+}
+
+function normalizeUrl(aUrl) {
+
+    if (aUrl.indexOf('http') < 0) {
+        if (aUrl.indexOf('/') !== 0) {
+            var path = url.parse(crawlUp(givenLink)).path;
+            aUrl = baseUrl + path + '/' + aUrl;
+ 
+        } else {
+            aUrl = baseUrl + aUrl;
+        }
+    }
+
+    return aUrl;
 }
